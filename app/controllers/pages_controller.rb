2bd6021c9ce8f5b8b720d090e1b3ea95177d1629
Annie Gilchrist <anneliese.gilchrist@gmail.com>
2019-01-21 21:16:26 +0000

    start building website

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,25 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def exhibitions
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def landscapes
+  end
+
+  def portraits
+  end
+
+  def still_life
+  end
+
+  def nudes
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'exhibitions', to: 'pages#exhibitions', as: :exhibitions
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'landscapes', to: 'pages#landscapes', as: :landscapes
+  get 'portraits', to: 'pages#portraits', as: :portraits
+  get 'still_life', to: 'pages#still_life', as: :still_life
+  get 'nudes', to: 'pages#nudes', as: :nudes
 end
